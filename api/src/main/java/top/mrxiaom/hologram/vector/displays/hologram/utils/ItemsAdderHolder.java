4a68af522a89ebb764903e4639ac071ba7e4a1ef
MrXiaoM <coolxiaom95@gmail.com>
2025-10-05 11:37:03 +0800

    remove unnecessary log

--- api/src/main/java/top/mrxiaom/hologram/vector/displays/hologram/utils/ItemsAdderHolder.java
+++ api/src/main/java/top/mrxiaom/hologram/vector/displays/hologram/utils/ItemsAdderHolder.java
@@ -3,13 +3,10 @@ package top.mrxiaom.hologram.vector.displays.hologram.utils;
 import dev.lone.itemsadder.api.FontImages.FontImageWrapper;
 import org.bukkit.Bukkit;
 
-import java.util.logging.Level;
-
 public class ItemsAdderHolder implements ReplaceText {
 
     public ItemsAdderHolder() throws ClassNotFoundException {
         if (Bukkit.getPluginManager().getPlugin("ItemsAdder") == null) {
-            Bukkit.getLogger().log(Level.WARNING, "[HologramAPI] ItemsAdder plugin not found! No custom emojis support.");
             throw new ClassNotFoundException();
         }
     }
