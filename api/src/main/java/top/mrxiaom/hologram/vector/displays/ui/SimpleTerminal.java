22cae8e0a773240d32e51aa2b1d66714f7f03400
MrXiaoM <coolxiaom95@gmail.com>
2025-10-09 12:05:12 +0800

    Update SimpleTerminal.java

--- api/src/main/java/top/mrxiaom/hologram/vector/displays/ui/SimpleTerminal.java
+++ api/src/main/java/top/mrxiaom/hologram/vector/displays/ui/SimpleTerminal.java
@@ -2,10 +2,14 @@ package top.mrxiaom.hologram.vector.displays.ui;
 
 import org.bukkit.Location;
 import org.jetbrains.annotations.NotNull;
+import top.mrxiaom.hologram.vector.displays.hologram.RenderMode;
 import top.mrxiaom.hologram.vector.displays.ui.api.Terminal;
 
 public class SimpleTerminal extends Terminal<SimpleTerminal> {
     public SimpleTerminal(@NotNull String id, @NotNull Location location, int widthSpace, int heightLines) {
         super(id, location, widthSpace, heightLines);
+    }
+    public SimpleTerminal(@NotNull RenderMode renderMode, @NotNull String id, @NotNull Location location, int widthSpace, int heightLines) {
+        super(renderMode, id, location, widthSpace, heightLines);
     }
 }
