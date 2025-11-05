336259ab611aa62868f79b40099119cd20223daa
MrXiaoM <coolxiaom95@gmail.com>
2025-11-05 18:52:38 +0800

    rename `root` to `parent`

--- api/src/main/java/top/mrxiaom/hologram/vector/displays/ui/EnumAlign.java
+++ api/src/main/java/top/mrxiaom/hologram/vector/displays/ui/EnumAlign.java
@@ -8,67 +8,67 @@ import org.bukkit.World;
  */
 @SuppressWarnings("CodeBlock2Expr")
 public enum EnumAlign {
-    LEFT_TOP((rootX, rootY, z, rootWidth, rootHeight, x, y, width, height) -> {
+    LEFT_TOP((parentX, parentY, z, parentWidth, parentHeight, x, y, width, height) -> {
         return new double[] {
-                left(rootX, rootWidth, x, width),
-                top(rootY, rootHeight, y, height),
+                left(parentX, parentWidth, x, width),
+                top(parentY, parentHeight, y, height),
                 z
         };
     }),
-    CENTER_TOP((rootX, rootY, z, rootWidth, rootHeight, x, y, width, height) -> {
+    CENTER_TOP((parentX, parentY, z, parentWidth, parentHeight, x, y, width, height) -> {
         return new double[] {
-                centerHorizontal(rootX, rootWidth, x, width),
-                top(rootY, rootHeight, y, height),
+                centerHorizontal(parentX, parentWidth, x, width),
+                top(parentY, parentHeight, y, height),
                 z
         };
     }),
-    RIGHT_TOP((rootX, rootY, z, rootWidth, rootHeight, x, y, width, height) -> {
+    RIGHT_TOP((parentX, parentY, z, parentWidth, parentHeight, x, y, width, height) -> {
         return new double[] {
-                right(rootX, rootWidth, x, width),
-                top(rootY, rootHeight, y, height),
+                right(parentX, parentWidth, x, width),
+                top(parentY, parentHeight, y, height),
                 z
         };
     }),
-    LEFT_CENTER((rootX, rootY, z, rootWidth, rootHeight, x, y, width, height) -> {
+    LEFT_CENTER((parentX, parentY, z, parentWidth, parentHeight, x, y, width, height) -> {
         return new double[] {
-                left(rootX, rootWidth, x, width),
-                centerVertical(rootY, rootHeight, y, height),
+                left(parentX, parentWidth, x, width),
+                centerVertical(parentY, parentHeight, y, height),
                 z
         };
     }),
-    CENTER((rootX, rootY, z, rootWidth, rootHeight, x, y, width, height) -> {
+    CENTER((parentX, parentY, z, parentWidth, parentHeight, x, y, width, height) -> {
         return new double[] {
-                centerHorizontal(rootX, rootWidth, x, width),
-                centerVertical(rootY, rootHeight, y, height),
+                centerHorizontal(parentX, parentWidth, x, width),
+                centerVertical(parentY, parentHeight, y, height),
                 z
         };
     }),
-    RIGHT_CENTER((rootX, rootY, z, rootWidth, rootHeight, x, y, width, height) -> {
+    RIGHT_CENTER((parentX, parentY, z, parentWidth, parentHeight, x, y, width, height) -> {
         return new double[] {
-                right(rootX, rootWidth, x, width),
-                centerVertical(rootY, rootHeight, y, height),
+                right(parentX, parentWidth, x, width),
+                centerVertical(parentY, parentHeight, y, height),
                 z
         };
     }),
-    LEFT_BOTTOM((rootX, rootY, z, rootWidth, rootHeight, x, y, width, height) -> {
+    LEFT_BOTTOM((parentX, parentY, z, parentWidth, parentHeight, x, y, width, height) -> {
         return new double[] {
-                left(rootX, rootWidth, x, width),
-                bottom(rootY, rootHeight, y, height),
+                left(parentX, parentWidth, x, width),
+                bottom(parentY, parentHeight, y, height),
                 z
         };
 
     }),
-    CENTER_BOTTOM((rootX, rootY, z, rootWidth, rootHeight, x, y, width, height) -> {
+    CENTER_BOTTOM((parentX, parentY, z, parentWidth, parentHeight, x, y, width, height) -> {
         return new double[] {
-                centerHorizontal(rootX, rootWidth, x, width),
-                bottom(rootY, rootHeight, y, height),
+                centerHorizontal(parentX, parentWidth, x, width),
+                bottom(parentY, parentHeight, y, height),
                 z
         };
     }),
-    RIGHT_BOTTOM((rootX, rootY, z, rootWidth, rootHeight, x, y, width, height) -> {
+    RIGHT_BOTTOM((parentX, parentY, z, parentWidth, parentHeight, x, y, width, height) -> {
         return new double[] {
-                right(rootX, rootWidth, x, width),
-                bottom(rootY, rootHeight, y, height),
+                right(parentX, parentWidth, x, width),
+                bottom(parentY, parentHeight, y, height),
                 z
         };
     });
@@ -84,36 +84,56 @@ public enum EnumAlign {
      * 的经典UI坐标系
      */
 
-    private static double left(double rootX, double rootWidth, double x, double width) {
-        return rootX - (rootWidth / 2) + x + (width / 2);
+    private static double left(double parentX, double parentWidth, double x, double width) {
+        return parentX - (parentWidth / 2) + x + (width / 2);
     }
-    private static double centerHorizontal(double rootX, double rootWidth, double x, double width) {
-        return rootX + x;
+    private static double centerHorizontal(double parentX, double parentWidth, double x, double width) {
+        return parentX + x;
     }
-    private static double right(double rootX, double rootWidth, double x, double width) {
-        return rootX + (rootWidth / 2) + x - (width / 2);
+    private static double right(double parentX, double parentWidth, double x, double width) {
+        return parentX + (parentWidth / 2) + x - (width / 2);
     }
-    private static double top(double rootY, double rootHeight, double y, double height) {
-        return rootY + rootHeight - y;
+    private static double top(double parentY, double parentHeight, double y, double height) {
+        return parentY + parentHeight - y;
     }
-    private static double centerVertical(double rootY, double rootHeight, double y, double height) {
-        return rootY + (rootHeight / 2) - y - (height / 2);
+    private static double centerVertical(double parentY, double parentHeight, double y, double height) {
+        return parentY + (parentHeight / 2) - y - (height / 2);
     }
-    private static double bottom(double rootY, double rootHeight, double y, double height) {
-        return rootY - y;
+    private static double bottom(double parentY, double parentHeight, double y, double height) {
+        return parentY - y;
     }
     interface Impl {
-        double[] get(double rootX, double rootY, double z, double rootWidth, double rootHeight, double x, double y, double width, double height);
+        double[] get(double parentX, double parentY, double z, double parentWidth, double parentHeight, double x, double y, double width, double height);
     }
     private final Impl impl;
     EnumAlign(Impl impl) {
         this.impl = impl;
     }
-    public double[] get(double rootX, double rootY, double z, double rootWidth, double rootHeight, double x, double y, double width, double height) {
-        return impl.get(rootX, rootY, z, rootWidth, rootHeight, x, y, width, height);
+
+    /**
+     * 获取经过对齐方式转换之后的三维坐标
+     * <p>
+     * 由此获得的是未经旋转的原始坐标
+     * @param parentX 父组件X坐标
+     * @param parentY 父组件Y坐标
+     * @param z Z坐标
+     * @param parentWidth 父组件宽度
+     * @param parentHeight 父组件高度
+     * @param x 目标组件X坐标
+     * @param y 目标组件Y坐标
+     * @param width 目标组件宽度
+     * @param height 目标组件高度
+     * @return 转换后的三维坐标
+     */
+    public double[] get(double parentX, double parentY, double z, double parentWidth, double parentHeight, double x, double y, double width, double height) {
+        return impl.get(parentX, parentY, z, parentWidth, parentHeight, x, y, width, height);
     }
-    public Location get(World world, double rootX, double rootY, double z, double rootWidth, double rootHeight, double x, double y, double width, double height) {
-        double[] raw = get(rootX, rootY, z, rootWidth, rootHeight, x, y, width, height);
+
+    /**
+     * @see EnumAlign#get(double, double, double, double, double, double, double, double, double)
+     */
+    public Location get(World world, double parentX, double parentY, double z, double parentWidth, double parentHeight, double x, double y, double width, double height) {
+        double[] raw = get(parentX, parentY, z, parentWidth, parentHeight, x, y, width, height);
         return new Location(world, raw[0], raw[1], raw[2]);
     }
 }
