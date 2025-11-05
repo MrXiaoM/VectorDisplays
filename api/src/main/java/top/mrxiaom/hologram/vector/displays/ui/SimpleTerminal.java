95370f990fe66fc12f18d0335bb2ab7e167c28ed
MrXiaoM <coolxiaom95@gmail.com>
2025-11-06 00:22:36 +0800

    sync SimpleTerminal with abstract Terminal

--- api/src/main/java/top/mrxiaom/hologram/vector/displays/ui/SimpleTerminal.java
+++ api/src/main/java/top/mrxiaom/hologram/vector/displays/ui/SimpleTerminal.java
@@ -12,4 +12,10 @@ public class SimpleTerminal extends Terminal<SimpleTerminal> {
     public SimpleTerminal(@NotNull RenderMode renderMode, @NotNull String id, @NotNull Location location, int widthSpace, int heightLines) {
         super(renderMode, id, location, widthSpace, heightLines);
     }
+    public SimpleTerminal(@NotNull String id, @NotNull Location location, double width, double height) {
+        super(id, location, width, height);
+    }
+    public SimpleTerminal(@NotNull RenderMode renderMode, @NotNull String id, @NotNull Location location, double width, double height) {
+        super(renderMode, id, location, width, height);
+    }
 }
