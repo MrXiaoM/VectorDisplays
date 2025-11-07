4b04eaa7aee9d3370dfe99385367485793d18cfb
MrXiaoM <coolxiaom95@gmail.com>
2025-11-07 15:04:11 +0800

    support to merge multiple hover state change

--- api/src/main/java/top/mrxiaom/hologram/vector/displays/ui/event/HoverStateChange.java
+++ api/src/main/java/top/mrxiaom/hologram/vector/displays/ui/event/HoverStateChange.java
@@ -18,7 +18,7 @@ public interface HoverStateChange<E extends Element<E, ?>> {
     void perform(boolean newState, E element);
 
     static <E extends Element<E, ?>> HoverStateChange<E> hoverBg(int hover, int normal) {
-        return hoverBg(hover, normal, null);
+        return hoverBg(hover, normal, HoverStateChange.<E>updateEntity());
     }
     static <E extends Element<E, ?>> HoverStateChange<E> hoverBg(int hover, int normal, @Nullable HoverStateChange<E> action) {
         return (newState, element) -> {
@@ -26,7 +26,6 @@ public interface HoverStateChange<E extends Element<E, ?>> {
                 txt.setBackgroundColor(newState ? hover : normal);
             }
             if (action != null) action.perform(newState, element);
-            else element.update();
         };
     }
     static <E extends Element<E, ?>> HoverStateChange<E> hoverBg(Material hover, Material normal) {
@@ -36,7 +35,7 @@ public interface HoverStateChange<E extends Element<E, ?>> {
         return hoverBg(new ItemStack(hover), new ItemStack(normal), action);
     }
     static <E extends Element<E, ?>> HoverStateChange<E> hoverBg(ItemStack hover, ItemStack normal) {
-        return hoverBg(hover, normal, null);
+        return hoverBg(hover, normal, HoverStateChange.<E>updateEntity());
     }
     static <E extends Element<E, ?>> HoverStateChange<E> hoverBg(ItemStack hover, ItemStack normal, @Nullable HoverStateChange<E> action) {
         return (newState, element) -> {
@@ -44,15 +43,14 @@ public interface HoverStateChange<E extends Element<E, ?>> {
                 txt.setItemStack(newState ? hover : normal);
             }
             if (action != null) action.perform(newState, element);
-            else element.update();
         };
     }
 
     static <E extends Element<E, ?>> HoverStateChange<E> hoverScale(float hover) {
-        return hoverScale(hover,null);
+        return hoverScale(hover, HoverStateChange.<E>updateEntity());
     }
     static <E extends Element<E, ?>> HoverStateChange<E> hoverScale(float hover, float normal) {
-        return hoverScale(hover, normal, null);
+        return hoverScale(hover, normal, HoverStateChange.<E>updateEntity());
     }
     static <E extends Element<E, ?>> HoverStateChange<E> hoverScale(float hover, @Nullable HoverStateChange<E> action) {
         return hoverScale(hover, 1.0f, action);
@@ -73,8 +71,20 @@ public interface HoverStateChange<E extends Element<E, ?>> {
                 display.setScale(scaleX * scale, scaleY * scale, scaleZ * scale);
             }
             if (action != null) action.perform(newState, element);
-            else element.update();
         };
+    }
+
+    @SafeVarargs
+    static <E extends Element<E, ?>> HoverStateChange<E> of(HoverStateChange<E>... events) {
+        return (newState, element) -> {
+            for (HoverStateChange<E> event : events) {
+                event.perform(newState, element);
+            }
+        };
+    }
+
+    static <E extends Element<E, ?>> HoverStateChange<E> updateEntity() {
+        return (newState, element) -> element.update();
     }
 
 }
