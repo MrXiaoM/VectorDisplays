a159a0a6cdec1bbdbe1204450f5bc59765838972
MrXiaoM <coolxiaom95@gmail.com>
2025-11-18 23:26:12 +0800

    add only enable specific nms options

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,16 @@
 rootProject.name = "VectorDisplays"
 
+val onlyEnableNMS: List<String> = listOf(
+    //"v1_20_R3"
+)
+
 include(":api")
 include(":nms")
 File(rootDir, "nms").listFiles()?.forEach { file ->
     if (File(file, "build.gradle.kts").exists()) {
-        include(":nms:${file.name}")
+        if (onlyEnableNMS.isEmpty() || onlyEnableNMS.contains(file.name)) {
+            include(":nms:${file.name}")
+        }
     }
 }
 include(":plugin")
